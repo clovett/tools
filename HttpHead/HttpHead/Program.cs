b7f8134ccb63299e37b1f325d26873d6d1400cf5
Chris Lovett <lovettchris@hotmail.com>
2013-11-26 10:30:45 -0800

    initial

---
+++ HttpHead/HttpHead/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpHead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(args[0]);
+                req.Method = "HEAD";
+
+                HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+                Console.WriteLine("Response: " + resp.StatusCode);
+                foreach (string key in resp.Headers.Keys)
+                {
+                    Console.WriteLine(key + "=" + resp.Headers[key]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("### Error: " + ex.Message);
+            }
+        }
+    }
+}
