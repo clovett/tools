8dbeef1043e3da27162ea9421b755579a548c695
Chris Lovett <lovettchris@hotmail.com>
2014-07-10 13:44:21 -0700

    Dashboard prototype

--- Dashboard/MainPage.xaml.cs
+++ Dashboard/MainPage.xaml.cs
@@ -5,13 +5,16 @@ using System.IO;
 using System.Linq;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
+using WinRTXamlToolkit.Controls.DataVisualization;
 using WinRTXamlToolkit.Controls.DataVisualization.Charting;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -29,8 +32,37 @@ namespace Dashboard
             this.Loaded += MainPage_Loaded;
         }
 
-        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        static TimeSpan AnimationDelay = TimeSpan.FromMilliseconds(200);
+
+        RadialDialDataValue CreateDialValue(double angle, string caption, string valueLabel)
         {
+            var context = new RadialDialDataValue()
+            {
+                Angle = angle,
+                Caption = caption,
+                ValueLabel = valueLabel
+            };
+
+            BeginAngleAnimation(context, 0, angle, AnimationDelay);
+            return context;
+        }
+
+        void BeginAngleAnimation(DependencyObject target, double from, double to, TimeSpan duration)
+        {
+            Storyboard sb = new Storyboard();
+            DoubleAnimation animation = new DoubleAnimation() { From = from, To = to, Duration = new Duration(duration) };
+            Storyboard.SetTarget(animation, target);
+            Storyboard.SetTargetProperty(animation, "(RadialDialDataValue.Angle)");
+            sb.Children.Add(animation);
+            animation.EnableDependentAnimation = true;
+            sb.Begin();
+        }
+
+
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {   
+            
             ObservableCollection<UsageData> context = new ObservableCollection<UsageData>();
             DateTime date = new DateTime(2013, 1, 1);
 
@@ -41,12 +73,17 @@ namespace Dashboard
             }
 
             ColumnSeries series = ChartByDay.Series.FirstOrDefault() as ColumnSeries;
-            series.ItemsSource = context;
+            series.ItemsSource = context;            
 
             DateTimeAxis axis = series.ActualIndependentAxis as DateTimeAxis;
             axis.Minimum = new DateTime(2013, 1, 1);
             axis.Maximum = new DateTime(2014, 1, 1);
             axis.Interval = 5;
+
+            UsesPerDayDial.DataContext = CreateDialValue(360.0 * (75.0 / 100.0), "used daily", "75");
+            ErrorsPerDayDial.DataContext = CreateDialValue(360.0 * (25.0 / 100.0), "service needed", "25");
+            DistributionDial.DataContext = CreateDialValue(360.0 * (60.0 / 100.0), "shared", "60");
+            UnusedPerDayDial.DataContext = CreateDialValue(360.0 * (10.0 / 100.0), "unused","10");
         }
 
         static double[] data = new double[] { 10, 15, 20, 22, 25, 29, 35, 45, 42, 40, 35, 30, 31, 32, 31, 30, 35, 40, 42, 44, 40, 35, 36, 42 };
---
+++ Dashboard/RadialDialDataValue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace Dashboard
+{
+
+    public class RadialDialDataValue : DependencyObject
+    {
+        public RadialDialDataValue()
+        {           
+            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+            {
+                Angle = 45;
+            }
+        }
+
+
+
+        public double Angle
+        {
+            get { return (double)GetValue(AngleProperty); }
+            set { SetValue(AngleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Angle.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AngleProperty =
+            DependencyProperty.Register("Angle", typeof(double), typeof(RadialDialDataValue), new PropertyMetadata(0.0));
+
+
+
+        public string Caption
+        {
+            get { return (string)GetValue(CaptionProperty); }
+            set { SetValue(CaptionProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Caption.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CaptionProperty =
+            DependencyProperty.Register("Caption", typeof(string), typeof(RadialDialDataValue), new PropertyMetadata(null));
+
+
+
+
+        public string ValueLabel
+        {
+            get { return (string)GetValue(ValueLabelProperty); }
+            set { SetValue(ValueLabelProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ValueLabel.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ValueLabelProperty =
+            DependencyProperty.Register("ValueLabel", typeof(string), typeof(RadialDialDataValue), new PropertyMetadata(null));
+
+        
+    }
+
+}
