3331f91db40475a269c9cb66fe85c855f1fbce2a
Chris Lovett <lovettchris@hotmail.com>
2015-03-28 18:56:53 -0700

    add more error handling.

--- OutlookSync/OutlookSync/App.xaml.cs
+++ OutlookSync/OutlookSync/App.xaml.cs
@@ -13,17 +13,38 @@ namespace OutlookSync
     /// </summary>
     public partial class App : Application
     {
+        
         public App()
         {
             this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(OnUnhandledException);            
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+
+        private void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            ShowUnhandledException(e.ExceptionObject);            
         }
 
         void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
-            UnhandledExceptionWindow uew = new UnhandledExceptionWindow();
-            uew.ErrorMessage = (e.Exception != null ? e.Exception.ToString() : "No Exception object provided");
             e.Handled = true;
-            uew.ShowDialog();
+            ShowUnhandledException(e.Exception);
+        }
+
+        void ShowUnhandledException(object e)
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                UnhandledExceptionWindow uew = new UnhandledExceptionWindow();
+                uew.ErrorMessage = (e != null ? e.ToString() : "No Exception object provided");
+                uew.ShowDialog();
+            }));
         }
     }
 }
