40843c29d817a30c543d88e615c4acc3eefee86e
Chris Lovett <lovettchris@hotmail.com>
2016-01-25 23:47:43 -0800

    Add handy too that remaps MyCloud share on login.

---
+++ MyCloudShare/MyCloudShare/NetworkResource.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCloudShare
+{
+    class NetworkResource
+    {
+        public static IEnumerable<NetResource> GetConnectedDrives()
+        {
+            IntPtr hEnum;
+
+            Int32 cbBuffer = 16384;
+            IntPtr buffer = Marshal.AllocCoTaskMem(cbBuffer);
+            if (buffer == IntPtr.Zero)
+            {
+                throw new OutOfMemoryException();
+            }
+            Int32 cEntries = -1;
+            Int32 entrySize = Marshal.SizeOf(typeof(NetResource));
+
+            int hr = WNetOpenEnum((Int32)ResourceScope.RESOURCE_CONNECTED, (Int32)ResourceType.RESOURCETYPE_DISK, (Int32)ResourceUsage.RESOURCEUSAGE_ALL, IntPtr.Zero, out hEnum);
+            if (hr != 0)
+            {
+                throw new Exception(string.Format("WNetOpenEnum failed {0}", hr.ToString("x")));
+            }
+            else
+            {
+                do
+                {
+                    hr = WNetEnumResource(hEnum, ref cEntries, buffer, ref cbBuffer);
+                    if (hr == 0)
+                    {
+                        for (int i = 0; i < cEntries; i++)
+                        {
+                            IntPtr ptr = buffer + (i * entrySize);
+                            NetResource nr = Marshal.PtrToStructure<NetResource>(ptr);
+                            yield return nr;
+                        }
+                    }
+                    else if (hr != ERROR_NO_MORE_ITEMS)
+                    {
+                        throw new Exception(String.Format("WNetEnumResource failed with error {0}", hr.ToString("x")));
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+                while (hr != ERROR_NO_MORE_ITEMS);
+            }
+
+            Marshal.FreeCoTaskMem(buffer);
+            WNetCloseEnum(hEnum);
+        }
+
+        internal static void RestoreConnection(string localDrive)
+        {
+            int hr = WNetRestoreSingleConnectionW(IntPtr.Zero, localDrive, true);
+            if (hr != 0)
+            {
+                throw new Exception(String.Format("WNetRestoreSingleConnectionW failed with error {0}", hr.ToString("x")));
+            }
+        }
+
+        internal static void ConnectShare(string localName, string remoteName)
+        {
+            NetResource res = new NetResource()
+            {
+                dwType = (Int32)ResourceType.RESOURCETYPE_DISK,
+                sLocalName = localName,
+                sRemoteName = remoteName
+            };
+            int hr = WNetAddConnection3(IntPtr.Zero, ref res, null, null, 0);
+            if (hr != 0)
+            {
+                throw new Exception(string.Format("WNetAddConnection3 failed with error {0}", hr.ToString("x")));
+            }
+        }
+
+        internal static void DisconnectLocalShare(string localName)
+        {
+            int hr = WNetCancelConnection(localName, true);
+            if (hr != 0)
+            {
+                throw new Exception(string.Format("WNetCancelConnection failed with error {0}", hr.ToString("x")));
+            }
+        }
+
+        const Int32 ERROR_NO_MORE_ITEMS = 259;
+
+        [DllImport("mpr.dll")]
+        internal static extern int WNetAddConnection3(IntPtr hwnd, ref NetResource pstNetRes, string psPassword, string psUsername, Int32 piFlags);
+
+        [DllImport("mpr.dll")]
+        internal static extern int WNetEnumResource(IntPtr hEnum, ref Int32 count, IntPtr buffer, ref Int32 bufferSize);
+
+        [DllImport("mpr.dll")]
+        internal static extern int WNetOpenEnum(Int32 scope, Int32 type, Int32 usage, IntPtr pstNetRes, out IntPtr hEnum);
+
+        [DllImport("mpr.dll")]
+        internal static extern int WNetCloseEnum(IntPtr hEnum);
+
+        [DllImport("mpr.dll")]
+        internal static extern int WNetCancelConnection(string lpName, bool fForce);
+
+        [DllImport("mpr.dll")]
+        internal static extern int WNetRestoreSingleConnectionW(IntPtr hwndParent, string lpDevice, bool fUseUI);
+    }
+
+    enum ResourceScope
+    {
+        RESOURCE_CONNECTED = 1,
+        RESOURCE_GLOBALNET = 2,
+        RESOURCE_REMEMBERED = 3,
+        RESOURCE_RECENT = 0x00000004,
+        RESOURCE_CONTEXT = 0x00000005
+    }
+
+    enum ResourceType
+    {
+        RESOURCETYPE_ANY = 0x00000000,
+        RESOURCETYPE_DISK = 0x00000001,
+        RESOURCETYPE_PRINT = 0x00000002,
+        RESOURCETYPE_RESERVED = 0x00000008,
+        //RESOURCETYPE_UNKNOWN  =  0xFFFFFFFF
+    }
+
+    enum ResourceUsage
+    {
+        RESOURCEUSAGE_CONNECTABLE = 0x00000001,
+        RESOURCEUSAGE_CONTAINER = 0x00000002,
+        RESOURCEUSAGE_NOLOCALDEVICE = 0x00000004,
+        RESOURCEUSAGE_SIBLING = 0x00000008,
+        RESOURCEUSAGE_ATTACHED = 0x00000010,
+        RESOURCEUSAGE_ALL = (RESOURCEUSAGE_CONNECTABLE | RESOURCEUSAGE_CONTAINER | RESOURCEUSAGE_ATTACHED)
+        //RESOURCEUSAGE_RESERVED      = 0x80000000
+    }
+
+    enum ResourceDisplayType
+    {
+        RESOURCEDISPLAYTYPE_GENERIC = 0x00000000,
+        RESOURCEDISPLAYTYPE_DOMAIN = 0x00000001,
+        RESOURCEDISPLAYTYPE_SERVER = 0x00000002,
+        RESOURCEDISPLAYTYPE_SHARE = 0x00000003,
+        RESOURCEDISPLAYTYPE_FILE = 0x00000004,
+        RESOURCEDISPLAYTYPE_GROUP = 0x00000005,
+        RESOURCEDISPLAYTYPE_NETWORK = 0x00000006,
+        RESOURCEDISPLAYTYPE_ROOT = 0x00000007,
+        RESOURCEDISPLAYTYPE_SHAREADMIN = 0x00000008,
+        RESOURCEDISPLAYTYPE_DIRECTORY = 0x00000009,
+        RESOURCEDISPLAYTYPE_TREE = 0x0000000A,
+        RESOURCEDISPLAYTYPE_NDSCONTAINER = 0x0000000B
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct NetResource
+    {
+        public int dwScope;
+        public int dwType;
+        public int dwDisplayType;
+        public int dwUsage;
+        public string sLocalName;
+        public string sRemoteName;
+        public string sComment;
+        public string sProvider;
+    }
+
+}
---
+++ MyCloudShare/MyCloudShare/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Sgml;
+using System.Xml.Linq;
+using System.Diagnostics;
+
+namespace MyCloudShare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+                string ip = FindIpAddress("familybackup", "admin", "inamberclad");
+
+                string share = @"\\" + ip + "\\Chris";
+                string local = "z:";
+                bool found = false;
+
+                // see if a netork drive is already mapped for this remote share.
+                foreach (var nr in NetworkResource.GetConnectedDrives())
+                {
+                    Console.WriteLine(nr.sLocalName + "=" + nr.sRemoteName);
+                    if (string.Compare(nr.sRemoteName, share, StringComparison.OrdinalIgnoreCase) == 0)
+                    {
+                        if (string.Compare(nr.sLocalName, local, StringComparison.OrdinalIgnoreCase) == 0)
+                        {
+                            // already mapped
+                            Console.WriteLine("Local drive {0} is already mapped to remote share {1}", local, share);
+                            return;
+                        }
+                        found = true;
+                    }
+                }
+
+                if (found)
+                {
+                    // need to remap the Z: drive to our remote share
+                    NetworkResource.DisconnectLocalShare(local);
+                }
+
+                // Map Z: drive to our remote share
+                NetworkResource.ConnectShare(local, share);
+
+                Console.WriteLine("Local drive {0} is successfully mapped to remote share {1}", local, share);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        static string FindIpAddress(string machineName, string username, string password)
+        {
+            XDocument doc = GetHtmlAsXml("http://192.168.1.1/DEV_device2.htm", username, password);
+            if (doc != null)
+            {
+
+                XElement e = (from node in doc.Descendants("span") where string.Compare(node.Value, machineName, StringComparison.OrdinalIgnoreCase) == 0 select node).FirstOrDefault();
+                if (e == null)
+                {
+                    throw new Exception(string.Format("machine '{0}' not found in connected device table", machineName));
+                }
+                XElement td = e.Parent;
+                XElement tr = td.Parent;
+                XElement s = (from node in tr.Descendants("span") where (string)node.Attribute("name") == "rule_ip" select node).FirstOrDefault();
+                if (s == null)
+                {
+                    throw new Exception(string.Format("rule_ip span missing for {0}'s tr element", machineName));
+                }
+                return s.Value;
+            }
+            return null;
+        }
+
+        static XDocument GetHtmlAsXml(string uri, string userid, string password)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(uri);
+            req.Credentials = new NetworkCredential(userid, password);
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            if (resp.StatusCode == HttpStatusCode.OK)
+            {
+                using (var stream = resp.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        using (SgmlReader sgml = new SgmlReader())
+                        {
+                            sgml.DocType = "html";
+                            sgml.InputStream = reader;
+                            sgml.CaseFolding = CaseFolding.ToLower;
+                            XDocument doc = XDocument.Load(sgml);
+                            return doc;
+                        }
+                    }
+                }
+            }
+            throw new Exception(string.Format("Request {0} failed: {1}", uri, resp.StatusDescription));
+        }
+
+        
+    }
+}
