34a00ca0a94b8db8ffd4244c181f8e65a0a23b63
Chris Lovett <lovettchris@hotmail.com>
2017-09-16 00:19:10 -0700

    fixes

--- VisionTools/batchtest/batchtest/main.cpp
+++ VisionTools/batchtest/batchtest/main.cpp
@@ -7,13 +7,17 @@
 int main(int argc, char** argv)
 {
 	// std::string val_map, std::string labels, std::string truthlabels, int top
-	if (argc != 5) {
+	if (argc <= 4) {
 		printf("Usage: batchtest val_map labels truthlabels top");
 		printf("e.g. batchtest d:\\temp\\cntk\\testset\val_map.txt darknetImageNetLabels.txt cntkVgg16ImageNetLabels.txt 5");
+		return 1;
 	}
 
 
-	int top = atoi(argv[4]);
+	int top = 1; 
+	if (argc > 4) {
+		top = atoi(argv[4]);
+	}
 
 	BatchTest test;
 	return test.Run(argv[1], argv[2], argv[3], top);
