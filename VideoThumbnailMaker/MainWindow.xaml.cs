2de9539dbca05f478cc2089f521e1a18b408aecc
Chris Lovett <clovett@Microsoft.com>
2018-11-07 12:03:51 -0800

    add cool WPF app template and beginnings of Video Thumbnail Maker

---
+++ VideoThumbnailMaker/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VideoThumbnailMaker.Utilities;
+
+namespace VideoThumbnailMaker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DelayedActions delayedActions = new DelayedActions();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            UiDispatcher.Initialize();
+
+            RestoreSettings();
+
+            this.SizeChanged += OnWindowSizeChanged;
+            this.LocationChanged += OnWindowLocationChanged;
+        }
+
+        private void OnOpenFile(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnClear(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnSettings(object sender, RoutedEventArgs e)
+        {
+            XamlExtensions.Flyout(AppSettingsPanel);
+        }
+
+        private void OnWindowLocationChanged(object sender, EventArgs e)
+        {
+            delayedActions.StartDelayedAction("SaveWindowLocation", SavePosition, TimeSpan.FromMilliseconds(1000));
+        }
+
+        private void OnWindowSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            delayedActions.StartDelayedAction("SaveWindowLocation", SavePosition, TimeSpan.FromMilliseconds(1000));
+        }
+
+        private async void RestoreSettings()
+        {
+            Settings settings = await ((App)App.Current).LoadSettings();
+            if (settings.WindowLocation.X != 0 && settings.WindowSize.Width != 0 && settings.WindowSize.Height != 0)
+            {
+                // make sure it is visible on the user's current screen configuration.
+                var bounds = new System.Drawing.Rectangle(
+                    XamlExtensions.ConvertFromDeviceIndependentPixels(settings.WindowLocation.X),
+                    XamlExtensions.ConvertFromDeviceIndependentPixels(settings.WindowLocation.Y),
+                    XamlExtensions.ConvertFromDeviceIndependentPixels(settings.WindowSize.Width),
+                    XamlExtensions.ConvertFromDeviceIndependentPixels(settings.WindowSize.Height));
+                var screen = System.Windows.Forms.Screen.FromRectangle(bounds);
+                bounds.Intersect(screen.WorkingArea);
+
+                this.Left = XamlExtensions.ConvertToDeviceIndependentPixels(bounds.X);
+                this.Top = XamlExtensions.ConvertToDeviceIndependentPixels(bounds.Y);
+                this.Width = XamlExtensions.ConvertToDeviceIndependentPixels(bounds.Width);
+                this.Height = XamlExtensions.ConvertToDeviceIndependentPixels(bounds.Height);
+            }
+            this.Visibility = Visibility.Visible;
+        }
+
+        async void SavePosition()
+        {
+            var bounds = this.RestoreBounds;
+
+            Settings settings = await ((App)App.Current).LoadSettings();
+            settings.WindowLocation = bounds.TopLeft;
+            settings.WindowSize = bounds.Size;
+            await settings.SaveAsync();
+        }
+    }
+}
---
+++ WpfAppTemplate/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfAppTemplate.Utilities;
+
+namespace WpfAppTemplate
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        Settings settings;
+
+        public async Task<Settings> LoadSettings()
+        {
+            if (this.settings == null)
+            {
+                this.settings = await Settings.LoadAsync();
+            }
+            return this.settings;
+        }
+    }
+}
