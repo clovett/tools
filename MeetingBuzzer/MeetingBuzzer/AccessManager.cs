b07d736b65975c3c39bd14c884e37c58a039cd36
Chris Lovett <clovett@microsoft.com>
2019-06-28 21:01:42 -0700

    beginnings of a meeting buzzer app !!

---
+++ MeetingBuzzer/MeetingBuzzer/AccessManager.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingBuzzer
+{
+    class AccessManager
+    {
+        string _accessToken;
+
+        public async Task<string> GetTokenAsync(string clientId)
+        {
+            if (string.IsNullOrEmpty(_accessToken))
+            {
+                var clientApp = PublicClientApplicationBuilder.Create(clientId);
+                var publicApp = clientApp.Build();
+
+                var accounts = await publicApp.GetAccountsAsync();
+                var firstAccount = accounts.FirstOrDefault();
+                var scopes = new string[] { "User.Read" };
+                try
+                {
+                    var parameterBuidler = publicApp.AcquireTokenSilent(scopes, firstAccount);
+                    var result = await parameterBuidler.ExecuteAsync();
+                    _accessToken = result.AccessToken;
+                }
+                catch (MsalUiRequiredException ex)
+                {
+                    var parameterBuidler = publicApp.AcquireTokenInteractive(scopes).WithAccount(firstAccount).WithPrompt(Prompt.SelectAccount);
+                    var result = await parameterBuidler.ExecuteAsync();
+                    _accessToken = result.AccessToken;
+                }
+            }
+            return _accessToken;
+        }
+
+    }
+}
---
+++ MeetingBuzzer/MeetingBuzzer/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security;
+using System.Threading.Tasks;
+using System.Windows;
+
+// see https://docs.microsoft.com/en-us/azure/active-directory/develop/tutorial-v2-windows-desktop
+
+namespace MeetingBuzzer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AccessManager accessManager = new AccessManager();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += OnMainWindowLoaded;
+        }
+
+        private async void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var token = accessManager.GetTokenAsync(ConfigurationManager.AppSettings["clientId"].ToString());
+
+                GraphServiceClient graphClient = new GraphServiceClient("https://graph.microsoft.com/v1.0", new DelegateAuthenticationProvider(async (requestMessage) => {
+                    requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", token);
+                }));
+
+                var currentUser = await graphClient.Me.Request().GetAsync();
+                Console.WriteLine(currentUser.DisplayName);
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    static class SecureStringExtensions
+    {
+        public static SecureString ToSecureString(this string s)
+        {
+            SecureString result = new SecureString();
+            foreach (var ch in s)
+            {
+                result.AppendChar(ch);
+            }
+            return result;
+        }
+    }
+}
