2cff5bb5c2fe1451b1b67f0f4607eac370cc663b
Chris Lovett <clovett@Microsoft.com>
2020-06-02 16:00:23 -0700

    add a version tool to edit common\version.props.

---
+++ version/version/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.IO;
+using System.Xml.Linq;
+
+namespace version
+{
+    class Program
+    {
+        const string versionFile = "Common\\version.props";
+
+        static void Main(string[] args)
+        {
+            string version = null;
+            if (args.Length > 0)
+            {
+                version = args[0];
+            }
+
+            if (File.Exists(versionFile))
+            {
+                XDocument doc = XDocument.Load(versionFile);
+                XElement prefix = doc.Root.Element("PropertyGroup").Element("VersionPrefix");
+                if (version == null)
+                {
+                    Console.WriteLine("Current version is: {0}", prefix.Value);
+                }
+                else if (version != prefix.Value)
+                {
+                    Console.WriteLine("Updating version to: {0}", version);
+                    prefix.SetValue(version);
+                    doc.Save(versionFile);
+                }
+                else
+                {
+                    Console.WriteLine("Version is already set to {0}", prefix.Value);
+                }
+            } 
+            else
+            {
+                Console.WriteLine("File not found: {0}", versionFile);
+            }
+        }
+    }
+}
