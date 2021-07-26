febbfbfa2195f853fc6cfed44af2edcb4ce3524f
Chris Lovett <lovettchris@hotmail.com>
2021-07-26 14:59:28 -0700

    add matching join command

---
+++ Split/Join/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextReader reader = null;
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: join characters [filename]");
+                Console.WriteLine("Splits all lines in the given input using the characters provided in first argument");
+                return;
+            }
+            string join = args[0];
+            if (args.Length == 1)
+            {
+                reader = Console.In;
+            }
+            else
+            {
+                reader = new StreamReader(args[1]);
+            }
+            while (true)
+            {
+                string line = reader.ReadLine();
+                if (line == null)
+                {
+                    break; // eof
+                }
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    Console.Write(line);
+                    Console.Write(join);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
