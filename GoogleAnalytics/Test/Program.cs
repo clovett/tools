c145e223af8a1339330b54738ead857fcd9633fc
Chris Lovett <lovettchris@hotmail.com>
2022-03-21 14:23:02 -0700

    Fix test app

--- GoogleAnalytics/Test/Program.cs
+++ GoogleAnalytics/Test/Program.cs
@@ -10,18 +10,20 @@ namespace Test
         {
             if (args.Length == 0)
             {
-                Console.WriteLine("Usage Test GoogleAnalyticsTrackingId");
+                Console.WriteLine("Usage Test Google Analytics Tracking Id");
+                return;
             }
 
+            string clientId = Guid.NewGuid().ToString(); 
             string trackingId = args[0];
 
             await GoogleAnalytics.HttpProtocol.PostMeasurements(new PageMeasurement()
             {
                 TrackingId = trackingId,
-                ClientId = "123",
+                ClientId = clientId,
                 HostName = "microsoft.github.io",
-                Path = "/XmlNotepad/help/clipboard",
-                Title = "Schemas"
+                Path = "/App/Launch",
+                Title = "Launch"
             });
 
             Console.WriteLine("measurement sent!!");
