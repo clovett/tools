594ee875ad0586c915d8af10fc2a3812c7021110
Chris Lovett <lovettchris@hotmail.com>
2022-03-23 19:39:51 -0700

    complete disable_autoshake.ps1

--- Scripts/disable_autoshake.ps1
+++ Scripts/disable_autoshake.ps1
@@ -1,1 +1,19 @@
-$gpmc = New-Object -ComObject GPMgmt.GPM
+# Install-Module -Name PolicyFileEditor 
+
+$UserDir = "$env:windir\system32\GroupPolicy\User\registry.pol"
+$RegPath = "Software\Policies\Microsoft\Windows\Explorer"
+$RegName = "NoWindowMinimizingShortcuts"
+$RegData = 0
+$RegType = "DWord"
+
+$x = Get-PolicyFileEntry -Path $UserDir -Key $RegPath -ValueName $RegName
+if ($x.Data -ne 0) {
+   Write-Host "disabling NoWindowMinimizingShortcuts..."
+   Set-PolicyFileEntry -Path $UserDir -Key $RegPath -ValueName $RegName -Data $RegData -Type $RegType
+   Write-Host "done"
+}
+else
+{
+    Write-Host $x
+    Write-Host "already disabled"
+}
