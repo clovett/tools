091ef76eb23e9bb6f80784c8621ac80b502538dd
clovett <lovettchris@hotmail.com>
2022-03-24 20:28:06 -0700

    add new setup script

---
+++ Scripts/setup_clovett.cmd
@@ -0,0 +1,18 @@
+@echo off
+set profile=%USERPROFILE%
+set "profile=%profile:\=/%"
+set keyfile=%profile%/.ssh/clovett
+if not exist "%keyfile%" goto :nokey
+if "%clovett_email%" == "" goto :noemail
+git config core.sshCommand "ssh -i %keyfile%"
+git config user.name %USERNAME%
+git config user.email "%clovett_email%"
+goto :eof
+:nokey
+echo Please configure your custom key in %keyfile%"
+echo using ssh-keygen -C %clovett_email% -f %keyfile%"
+echo then register that on your github account.
+goto :eof
+:noemail
+echo Please configure your 'clovett_email' environment variable.
+goto :eof
