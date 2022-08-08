e187c3f0b1c2473f035ce1acdf7eb267e8b3aede
Chris Lovett <lovettchris@hotmail.com>
2022-08-08 12:34:02 -0700

    add handy tool for showing toast notifications.

---
+++ ToastMe/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string message = "this is a test";
+        if (args.Length > 0)
+        {
+            message = args[0];
+        }
+
+        // Requires Microsoft.Toolkit.Uwp.Notifications NuGet package version 7.0 or greater
+        new ToastContentBuilder()
+            .AddText(message)
+            .Show(); // Not seeing the Show() method? Make sure you have version 7.0, and if you're using .NET 6 (or later), then your TFM must be net6.0-windows10.0.17763.0 or greater
+    }
+}
