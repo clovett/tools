8cbbe1235a8619614057b20bbc247e208b2d1556
Chris Lovett <lovettchris@hotmail.com>
2022-08-08 14:10:46 -0700

    fix grep

--- ToastMe/monitor_ssh_ps.ps1
+++ ToastMe/monitor_ssh_ps.ps1
@@ -8,9 +8,15 @@ param(
 )
 
 while ($True){
-    $rc = &ssh $machine ps ax | grep $name
-    $len = $rc.Length
-    if ($rc.Length -lt $count) {
+    $rc = &ssh $machine ps ax
+    $found = 0
+    foreach ($line in $rc){
+        if ($line.Contains($name)) {
+            $found = $found + 1
+        }
+    }
+    Write-Host "Found $found $name"
+    if ($found -lt $count) {
         &ToastMe "$machine does not have $count instances of $name"
         Exit 1
     }
