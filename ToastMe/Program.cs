02f04150430a4c04ebadb9a3dd672735efc5a022
clovett <lovettchris@hotmail.com>
2022-08-10 12:29:15 -0700

    fixes

--- ToastMe/Program.cs
+++ ToastMe/Program.cs
@@ -14,5 +14,7 @@ class Program
         new ToastContentBuilder()
             .AddText(message)
             .Show(); // Not seeing the Show() method? Make sure you have version 7.0, and if you're using .NET 6 (or later), then your TFM must be net6.0-windows10.0.17763.0 or greater
+
+        Thread.Sleep(1000);
     }
 }
