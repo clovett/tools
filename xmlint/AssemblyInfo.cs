4544242cd9008c48502cfc954cd4731f82911eac
Chris Lovett <lovettchris@hotmail.com>
2023-01-25 14:37:22 -0800

    Migrate to dotnet project format

--- xmlint/AssemblyInfo.cs
+++ xmlint/AssemblyInfo.cs
@@ -2,33 +2,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 //
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-//
-[assembly: AssemblyTitle("xmlint")]
-[assembly: AssemblyDescription("XML LINT tool for Managed Code")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft Corporation")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Microsoft Corporation")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]		
-
-//
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("1.0.*")]
-
-//
 // In order to sign your assembly you must specify a key to use. Refer to the 
 // Microsoft .NET Framework documentation for more information on assembly signing.
 //
