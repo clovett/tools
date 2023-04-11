f3a143a34df882b3c73e0d3d82ce28f14e893009
Chris Lovett <lovettchris@hotmail.com>
2023-04-10 21:41:31 -0700

    dox validator

---
+++ DocxValidator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Validation;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace DocxValidator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: validate a docx file");
+                return;
+            }
+            ValidateWordDocument(args[0]);
+        }
+
+        public static void ValidateWordDocument(string filepath)
+        {
+            using (var wordprocessingDocument = WordprocessingDocument.Open(filepath, true))
+            { 
+                try
+                {
+                    var validator = new OpenXmlValidator();
+                    int count = 0;
+                    foreach (var error in validator.Validate(wordprocessingDocument))
+                    {
+                        count++;
+                        Console.WriteLine("Error " + count);
+                        Console.WriteLine("Description: " + error.Description);
+                        Console.WriteLine("ErrorType: " + error.ErrorType);
+                        Console.WriteLine("Node: " + error.Node);
+                        Console.WriteLine("Path: " + error.Path.XPath);
+                        Console.WriteLine("Part: " + error.Part.Uri);
+                        Console.WriteLine("-------------------------------------------");
+                    }
+
+                    Console.WriteLine("count={0}", count);
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                wordprocessingDocument.Close();
+            }
+        }
+
+    }
+}
