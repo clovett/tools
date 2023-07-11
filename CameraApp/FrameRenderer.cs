78ed31eec5a392d6b1f74972574157f4c1d00710
Chris Lovett <lovettchris@hotmail.com>
2023-07-11 14:40:36 -0700

    simple webcam recorder.

---
+++ CameraApp/FrameRenderer.cs
@@ -0,0 +1,181 @@
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the Microsoft Public License.
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture;
+using Windows.Media.Capture.Frames;
+using Windows.Media.MediaProperties;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace CameraApp
+{
+    [ComImport]
+    [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    unsafe interface IMemoryBufferByteAccess
+    {
+        void GetBuffer(out byte* buffer, out uint capacity);
+    }
+
+    class FrameRenderer
+    {
+        private Image _imageElement;
+        private SoftwareBitmap _backBuffer;
+        private bool _taskRunning = false;
+        MediaFrameReader _sourceReader;
+
+        public FrameRenderer(Image imageElement)
+        {
+            _imageElement = imageElement;
+            _imageElement.Source = new SoftwareBitmapSource();
+        }
+
+        public async Task OpenReader(MediaCapture capture, MediaFrameSource source, string requestedSubType)
+        {
+            MediaFrameReader frameReader = await capture.CreateFrameReaderAsync(source, requestedSubType);
+            frameReader.FrameArrived += FrameReader_FrameArrived;
+            _sourceReader = frameReader;
+
+            MediaFrameReaderStartStatus status = await frameReader.StartAsync();
+            if (status != MediaFrameReaderStartStatus.Success)
+            {
+                throw new Exception($"Unable to start frame reader. Error: {status}");
+            }
+        }
+
+        private void FrameReader_FrameArrived(MediaFrameReader sender, MediaFrameArrivedEventArgs args)
+        {
+            // TryAcquireLatestFrame will return the latest frame that has not yet been acquired.
+            // This can return null if there is no such frame, or if the reader is not in the
+            // "Started" state. The latter can occur if a FrameArrived event was in flight
+            // when the reader was stopped.
+            using (var frame = sender.TryAcquireLatestFrame())
+            {
+                if (frame != null)
+                {
+                    this.ProcessFrame(frame);
+                }
+            }
+        }
+
+        public void ProcessFrame(MediaFrameReference frame)
+        {
+            var softwareBitmap = FrameRenderer.ConvertToDisplayableImage(frame?.VideoMediaFrame);
+
+            RenderFrame(softwareBitmap);
+        }
+
+        public void RenderFrame(SoftwareBitmap softwareBitmap)
+        {
+            if (softwareBitmap != null)
+            {
+                // Swap the processed frame to _backBuffer and trigger UI thread to render it
+                softwareBitmap = Interlocked.Exchange(ref _backBuffer, softwareBitmap);
+
+                // UI thread always reset _backBuffer before using it.  Unused bitmap should be disposed.
+                softwareBitmap?.Dispose();
+
+                // Changes to xaml ImageElement must happen in UI thread through Dispatcher
+                var task = _imageElement.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                    async () =>
+                    {
+                        // Don't let two copies of this task run at the same time.
+                        if (_taskRunning)
+                        {
+                            return;
+                        }
+                        _taskRunning = true;
+
+                        // Keep draining frames from the backbuffer until the backbuffer is empty.
+                        SoftwareBitmap latestBitmap;
+                        while ((latestBitmap = Interlocked.Exchange(ref _backBuffer, null)) != null)
+                        {
+                            var imageSource = (SoftwareBitmapSource)_imageElement.Source;
+                            await imageSource.SetBitmapAsync(latestBitmap);
+                            latestBitmap.Dispose();
+                        }
+
+                        _taskRunning = false;
+                    });
+            }
+        }
+        // Function delegate that transforms a scanline from an input image to an output image.
+        private unsafe delegate void TransformScanline(int pixelWidth, byte* inputRowBytes, byte* outputRowBytes);
+
+        /// <summary>
+        /// Determines the subtype to request from the MediaFrameReader that will result in
+        /// a frame that can be rendered by ConvertToDisplayableImage.
+        /// </summary>
+        /// <returns>Subtype string to request, or null if subtype is not renderable.</returns>
+        public static string GetSubtypeForFrameReader(MediaFrameSourceKind kind, MediaFrameFormat format)
+        {
+            // Note that media encoding subtypes may differ in case.
+            // https://docs.microsoft.com/en-us/uwp/api/Windows.Media.MediaProperties.MediaEncodingSubtypes
+            string subtype = format.Subtype;
+            switch (kind)
+            {
+                // For color sources, we accept anything and request that it be converted to Bgra8.
+                case MediaFrameSourceKind.Color:
+                    return MediaEncodingSubtypes.Bgra8;
+                // No other source kinds are supported by this class.
+                default:
+                    return null;
+            }
+        }
+
+        /// <summary>
+        /// Converts a frame to a SoftwareBitmap of a valid format to display in an Image control.
+        /// </summary>
+        /// <param name="inputFrame">Frame to convert.</param>
+        private static unsafe SoftwareBitmap ConvertToDisplayableImage(VideoMediaFrame inputFrame)
+        {
+            SoftwareBitmap result = null;
+            using (var inputBitmap = inputFrame?.SoftwareBitmap)
+            {
+                if (inputBitmap != null)
+                {
+                    switch (inputFrame.FrameReference.SourceKind)
+                    {
+                        case MediaFrameSourceKind.Color:
+                            // XAML requires Bgra8 with premultiplied alpha.
+                            // We requested Bgra8 from the MediaFrameReader, so all that's
+                            // left is fixing the alpha channel if necessary.
+                            if (inputBitmap.BitmapPixelFormat != BitmapPixelFormat.Bgra8)
+                            {
+                                Debug.WriteLine("Color frame in unexpected format.");
+                            }
+                            else if (inputBitmap.BitmapAlphaMode == BitmapAlphaMode.Premultiplied)
+                            {
+                                // Already in the correct format.
+                                result = SoftwareBitmap.Copy(inputBitmap);
+                            }
+                            else
+                            {
+                                // Convert to premultiplied alpha.
+                                result = SoftwareBitmap.Convert(inputBitmap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+                            }
+                            break;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ CameraApp/MainPage.xaml.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Media.Capture;
+using Windows.Media.Capture.Frames;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CameraApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private MediaCapture _mediaCapture;
+        private FrameRenderer _frameRenderer;
+        bool _recording;
+        bool _paused;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.UpdateButtonState();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var allGroups = await MediaFrameSourceGroup.FindAllAsync();
+            if (allGroups.Count == 0)
+            {
+                ShowStatus("No source groups found.");
+                return;
+            }
+            MediaFrameSourceGroup selectedGroup = null;
+
+            foreach (var group in allGroups)
+            {
+                // MediaFrameSourceKind kind = source.Info.SourceKind;
+                selectedGroup = group;
+                _mediaCapture = new MediaCapture();
+                var settings = new MediaCaptureInitializationSettings
+                {
+                    SourceGroup = group,
+
+                    // This media capture can share streaming with other apps.
+                    SharingMode = MediaCaptureSharingMode.SharedReadOnly,
+
+                    // Only stream video and don't initialize audio capture devices.
+                    StreamingCaptureMode = StreamingCaptureMode.Video,
+
+                    // Set to CPU to ensure frames always contain CPU SoftwareBitmap images
+                    // instead of preferring GPU D3DSurface images.
+                    MemoryPreference = MediaCaptureMemoryPreference.Cpu
+                };
+
+                await _mediaCapture.InitializeAsync(settings);
+                break;
+            }
+
+            var startedKinds = new HashSet<MediaFrameSourceKind>();
+            foreach (MediaFrameSource source in _mediaCapture.FrameSources.Values)
+            {
+                MediaFrameSourceKind kind = source.Info.SourceKind;
+                if (kind != MediaFrameSourceKind.Color)
+                {
+                    continue;
+                }
+
+                // Ignore this source if we already have a source of this kind.
+                if (startedKinds.Contains(kind))
+                {
+                    continue;
+                }
+
+                // Look for a format which the FrameRenderer can render.
+                string requestedSubtype = null;
+                foreach (MediaFrameFormat format in source.SupportedFormats)
+                {
+                    requestedSubtype = FrameRenderer.GetSubtypeForFrameReader(kind, format);
+                    if (requestedSubtype != null)
+                    {
+                        // Tell the source to use the format we can render.
+                        // await source.SetFormatAsync(format);
+                        break;
+                    }
+                }
+                if (requestedSubtype == null)
+                {
+                    // No acceptable format was found. Ignore this source.
+                    continue;
+                }
+
+                this._frameRenderer = new FrameRenderer(this.CameraFrame);
+                try
+                {
+                    await this._frameRenderer.OpenReader(_mediaCapture, source, requestedSubtype);
+                    startedKinds.Add(kind);
+                    ShowStatus($"Started {kind} reader.");
+                } 
+                catch (Exception ex)
+                {
+                    ShowStatus(ex.Message);
+                    return;
+                }
+            }
+
+            if (startedKinds.Count == 0)
+            {
+                ShowStatus($"No eligible sources in {selectedGroup.DisplayName}.");
+            }
+        }
+
+        private void ShowStatus(string message)
+        {
+            this.Status.Text = message;
+        }
+
+        private async void OnRecord(object sender, RoutedEventArgs e)
+        {
+            if (_paused && _recording)
+            {
+                await _mediaCapture.ResumeRecordAsync();
+                _paused = false;
+                UpdateButtonState();
+                return;
+            }
+
+            try
+            {
+                await StartRecording();
+                _recording = true;
+                _paused = false;
+                UpdateButtonState();
+            } 
+            catch (Exception ex)
+            {
+                ShowStatus(ex.Message);
+            }
+
+        }
+        LowLagMediaRecording _mediaRecording;
+
+        private async Task StartRecording()
+        {
+            // Use the MP4 preset to an obtain H.264 video encoding profile
+            //var mep = MediaEncodingProfile.CreateMp4(VideoEncodingQuality.HD1080p);
+            //mep.Audio = null;
+            //mep.Container = null;
+            ////if (previewEncodingProperties != null)
+            //{
+            //    mep.Video.Width = 1280;
+            //    mep.Video.Height = 720;
+            //}
+
+            //StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            //StorageFile videoFile = await storageFolder.CreateFileAsync("recording.mp4", CreationCollisionOption.ReplaceExisting);
+            //await _mediaCapture.StartRecordToStorageFileAsync(mep, videoFile);
+
+            var myVideos = await Windows.Storage.StorageLibrary.GetLibraryAsync(Windows.Storage.KnownLibraryId.Videos);
+            StorageFile file = await myVideos.SaveFolder.CreateFileAsync("recording.mp4", CreationCollisionOption.ReplaceExisting);
+            _mediaRecording = await _mediaCapture.PrepareLowLagRecordToStorageFileAsync(
+                    MediaEncodingProfile.CreateMp4(VideoEncodingQuality.Auto), file);
+            await _mediaRecording.StartAsync();
+
+            ShowStatus("Writing to: " + file.Path);
+        }
+
+        private async void OnPause(object sender, RoutedEventArgs e)
+        {
+            if (_recording)
+            {
+                _paused = true;
+                UpdateButtonState();
+                await _mediaRecording.PauseAsync(Windows.Media.Devices.MediaCapturePauseBehavior.RetainHardwareResources);
+                ShowStatus("Paused");
+            }
+        }
+
+        private async void OnStop(object sender, RoutedEventArgs e)
+        {
+            if (_recording)
+            {
+                _paused = false;
+                _recording = false;
+                UpdateButtonState();
+                await _mediaRecording.StopAsync();
+                ShowStatus("Recording stopped");
+            }
+        }
+
+        void UpdateButtonState()
+        {
+            RecordButton.Visibility = _recording && !_paused ? Visibility.Collapsed : Visibility.Visible;
+            PauseButton.Visibility = _recording && !_paused ? Visibility.Visible : Visibility.Collapsed;
+            StopButton.Visibility = _recording ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+    }
+}
